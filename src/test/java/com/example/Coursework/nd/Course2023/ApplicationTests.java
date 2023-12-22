50839a704fd49fd86489fd5a397896d3e143edbd
khilola.kushbakova <xilola_kushbakova@mai.ru>
2023-12-22 18:43:54 +0200

    Initial commit

---
+++ src/test/java/com/example/Coursework/nd/Course2023/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Coursework.nd.Course2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
